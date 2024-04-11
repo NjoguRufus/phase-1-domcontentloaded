bd8d41e3fd4d6b895f7c5b2e70241293216e84e4
NjoguRufus <rufus.njogu@student.moringaschool.com>
2024-04-11 12:53:32 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    // Target the paragraph element with id="text"
+    const textElement = document.getElementById("text");
+  
+    // Replace text with "This is really cool!"
+    textElement.textContent = "This is really cool!";
+  });
